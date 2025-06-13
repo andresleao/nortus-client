2c0f3f0994494ea3e1f8ecd024784e6a49e13d82
André Leão <andrefelipesl123@gmail.com>
2025-06-13 19:02:04 -0300

    Projeto criado

---
+++ components/header.tsx
@@ -0,0 +1,11 @@
+export default function Header() {
+    return (
+      <div className="w-full h-[88px] bg-[#20273E]">
+        <div className="ml-[var(--side-menu-width)] px-[40px] flex items-center h-full">
+          <span className="font-semibold text-[20px] leading-[24px] text-[#F6F8FC]">
+            Visão 360º
+          </span>
+        </div>
+      </div>
+    );
+  }
---
+++ components/side-menu.tsx
@@ -0,0 +1,7 @@
+export default function SideMenu() {
+    return (
+        <div className="h-screen w-[var(--side-menu-width)] fixed top-0 left-0 z-50 bg-[#20273E] rounded-tr-[40px] rounded-br-[40px] [filter:drop-shadow(4px_0_20px_rgba(0,0,0,0.2))]">
+            Side Menu
+        </div>
+    );
+}
