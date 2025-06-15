bafe3da95599febe3df4102889ba24468b33a0a4
André Leão <andrefelipesl123@gmail.com>
2025-06-15 10:10:56 -0300

    Tabela de Ofertas

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,9 +1,11 @@
+import ActiveOffersSection from "@/components/dashboard/active-offers-section";
 import KpiSection from "@/components/dashboard/kpi-section";
 
 export default function Dashboard() {
     return (
-        <div className="mt-[--header-height] pt-[47px] flex flex-col items-center w-full">
+        <div className="mt-[--header-height] pt-[47px] flex flex-col items-center w-full mb-[40px]">
             <KpiSection />
+            <ActiveOffersSection />
         </div>
     );
 }
--- components/icon-button.tsx
+++ components/icon-button.tsx
@@ -7,30 +7,47 @@ type IconButtonProps = {
   alt: string;
   href?: string;
   isActive?: boolean;
-
+  backgroundColor?: string;
+  radius?: string;
+  size?: string;
+  iconSize?: number;
 };
 
-export default function IconButton({ src, alt, href, isActive = false }: IconButtonProps) {
-  const buttonClasses = clsx(
-    'w-16 h-[60px] rounded-[12px] flex items-center justify-center transition-all duration-200',
-    {
-      'bg-[#1876D2] [filter:drop-shadow(0_0_10px_#1876D2)]': isActive,
-      'bg-white/5': !isActive,
-      'cursor-pointer': href
-    }
-  );
-
-  if (!href) {
-    return (
-      <div className={buttonClasses}>
-        <Image src={src} alt={alt} width={24} height={24} priority />
-      </div>
+export default function IconButton({
+    src,
+    alt,
+    href,
+    isActive = false,
+    backgroundColor,
+    radius = "rounded-[12px]",
+    size = "w-16 h-[60px]",
+    iconSize = 24
+}: IconButtonProps) {
+    const buttonClasses = clsx(
+        radius,
+        size,
+        "flex items-center justify-center transition-all duration-200",
+        {
+            "bg-[#1876D2] [filter:drop-shadow(0_0_10px_#1876D2)]": isActive,
+            "bg-white/5": !isActive,
+            "cursor-pointer": href
+        },
     );
-  }
 
-  return (
-    <Link href={href} className={buttonClasses}>
-      <Image src={src} alt={alt} width={24} height={24} priority />
-    </Link>
-  );
+    if (!href) {
+        return (
+            <div
+            className={buttonClasses}
+            style={ backgroundColor ? { backgroundColor } : undefined }
+            >
+            <Image src={src} alt={alt} width={iconSize} height={iconSize} priority />
+            </div>
+        );
+    }
+
+    return (
+        <Link href={href} className={buttonClasses}>
+            <Image src={src} alt={alt} width={iconSize} height={iconSize} priority />
+        </Link>
+    );
 }
