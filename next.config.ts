782e064960587e9fcb77ab92d582c04735e68675
André Leão <andrefelipesl123@gmail.com>
2025-06-17 22:33:51 -0300

    Deploy configs

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  
+   output: 'export',
+  basePath: '/nortus-client',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
